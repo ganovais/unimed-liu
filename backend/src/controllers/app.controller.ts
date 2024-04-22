b845f98249fd94d5e7967f6d6c6e654f0f631cf1
Gabriel Novais <gabriel_novais1997@hotmail.com>
2024-04-22 19:26:34 -0300

    Prisma configuration

--- backend/src/controllers/app.controller.ts
+++ backend/src/controllers/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../services/app.service';
 
 @Controller()
 export class AppController {
